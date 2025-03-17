354fa5c916f32bdb53098b5b70b73f578d5ff8dd
Pramisha <pramisha1209@gmail.com>
2025-03-18 01:14:34 +0545

    c

--- server/server.js
+++ server/server.js
@@ -100,8 +100,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 // Routes
 app.use('/api/auth', authRouter);
 app.use('/api/admin/products', adminProductsRouter);
